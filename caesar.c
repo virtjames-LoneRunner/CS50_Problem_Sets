ce2309fd3b1afbea11ff8de0f7af914075d6a0e8
virtjames-LoneRunner <virtjames-LoneRunner@users.noreply.github.com>
2020-08-17 09:02:23 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -73,6 +73,12 @@ int main(int argc, string argv[])
         printf("Usage: ./ceasar key \n");
         return 1;
     }
+    else
+    {
+        printf("Usage: ./ceasar key \n");
+        return 1;
+    }
+    printf("\n");
 
 
 
