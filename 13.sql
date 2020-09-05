4203f35e067923bf306220413a659a1b8bfe7120
virtjames-LoneRunner <virtjames-LoneRunner@users.noreply.github.com>
2020-09-06 00:53:45 +0800

    automated commit by submit50 [lang=en_PH:en]

--- 13.sql
+++ 13.sql
@@ -1,4 +1,4 @@
-SELECT COUNT(DISTINCT name) FROM people
+SELECT DISTINCT name FROM people
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON stars.movie_id = movies.id
 WHERE movies.title IN (SELECT DISTINCT movies.title FROM people
