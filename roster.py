f3fc0ccd5cbf9f385baff0a28211bf583ebaa2d9
virtjames-LoneRunner <virtjames-LoneRunner@users.noreply.github.com>
2020-09-06 22:32:09 +0800

    automated commit by submit50 [lang=en_PH:en]

--- roster.py
+++ roster.py
@@ -15,17 +15,23 @@ db = SQL("sqlite:///students.db")
 # Query students.db for student data
 list_of_students =  db.execute("SELECT first, middle, last, birth FROM students WHERE house = ? ORDER BY last, first", house)
 
+# If the query does not return anything then the house argument is wrong
 if list_of_students == []:
-    print("House not found")
+    print("House not found. Houses: Gryffindor, Hufflepuff, Ravenclaw, Slytherin")
+
+# If query is successful check middle name for content
 else:
+    # Iterate through the list
     for item in list_of_students:
         first = item["first"]
         middle = item["middle"]
         last = item["last"]
         birth = item["birth"]
 
+        # If there is no middle name then exclude
         if middle == None:
             print(first + " " + last + ", born " + str(birth))
 
+        # If there is a middle name then include
         else:
             print(first + " " + middle + " " + last + ", born " + str(birth))
